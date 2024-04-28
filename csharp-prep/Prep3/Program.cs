b3ad9218a2d65f8e431d7278f8a475e229cc2cc4
Ezequiel Espada <e.h.espada@gmail.com>
2024-04-28 03:23:44 -0300

    prep 03 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        int userNumber = 9999;
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("I have a magic number");
+        Console.WriteLine("Try to guess my magic number");
+      
+        while (userNumber != magicNumber)
+        {
+            Console.Write("What is you guess? ");
+            string userStringNumber = Console.ReadLine();
+            userNumber = int.Parse(userStringNumber);
+        
+            if (userNumber > magicNumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else if (userNumber < magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+        }
+        if (userNumber == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
     }
 }
